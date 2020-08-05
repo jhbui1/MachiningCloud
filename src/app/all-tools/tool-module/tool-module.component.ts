f4deb8c5cb44780918a071e1d0a3eed4b96fd260
jhbui1 <jhbui1@uci.edu>
2020-08-04 17:02:21 -0700

    handle changing module name within ToolModuleComponent

--- src/app/all-tools/tool-module/tool-module.component.ts
+++ src/app/all-tools/tool-module/tool-module.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { ToolsService, Tool } from 'src/app/tools.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { tap, share, switchMap } from 'rxjs/operators';
 
 @Component({
@@ -10,41 +10,62 @@ import { tap, share, switchMap } from 'rxjs/operators';
   templateUrl: './tool-module.component.html',
   styleUrls: ['./tool-module.component.scss']
 })
-export class ToolModuleComponent implements OnInit {
-  tileName: string;
-  tools$: Observable<Tool[]>
+export class ToolModuleComponent implements OnInit, OnDestroy {
+  public tools$: Observable<Tool[]>
+
+  private urlSubscription: Subscription | null = null;
 
   constructor(
     private location: Location,
     private router: Router,
+    private route: ActivatedRoute,
     private toolsService: ToolsService
   ) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.urlSubscription = this.route.url.subscribe( (urlSegments) => {
+      this.retrieveModuleTool()
+    })
+  }
+
+  public ngOnDestroy(): void {
+    this.urlSubscription.unsubscribe();
+  }
+
+  public home() {
+    this.location.back();
+  }
+
+  /**
+   * Parses url to initiate service calls for verification of module name
+   */
+  private retrieveModuleTool() {
     const url = this.router.url.split("/");
     const moduleName = url[url.length-1]
 
-    debugger;
     const toolsWithModuleNameAsName = this.toolsService.GetToolsWithName(moduleName);
     const toolsWithModuleNameAsParent = this.toolsService.GetToolsWithParent(moduleName);
 
-    this.tools$ = toolsWithModuleNameAsParent.pipe(
+    this.tools$ = this.verifyModuleTool(toolsWithModuleNameAsName,toolsWithModuleNameAsParent)
+  }
+
+  /**
+   * verifies modulename is has exactyl one tool with matching name and is not a parent of any tools
+   * @param toolsWithModuleNameAsName tools having given module name
+   * @param toolsWithModuleNameAsParent tools having given module name as parent
+   */
+  private verifyModuleTool(toolsWithModuleNameAsName: Observable<Tool[]>, toolsWithModuleNameAsParent: Observable<Tool[]>): Observable<Tool[]> {
+    return toolsWithModuleNameAsParent.pipe(
       switchMap( matchingParentTools => {
         return toolsWithModuleNameAsName.pipe(
           tap(matchingNamedTools => {
-            debugger;
-            if(matchingNamedTools.length != 1 || matchingParentTools.length !=0) {//module should have unique name and should not be a parent
+            if(matchingNamedTools.length != 1 || matchingParentTools.length != 0) {
               this.router.navigate(['not-found'])
             }
           }),
           share()
         )
-      })
-    )
-
+      }));
   }
 
-  home() {
-    this.location.back();
-  }
 }
